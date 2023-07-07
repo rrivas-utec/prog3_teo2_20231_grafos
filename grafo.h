eb0000834ef75d1ac298279c641b4e55b5de5b41
rrivas <rrivas@utec.edu.pe>
2023-07-06 21:56:05 -0500

    update repo

--- grafo.h
+++ grafo.h
@@ -9,24 +9,38 @@
 #include <unordered_map>
 #include <vector>
 #include <forward_list>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <algorithm>
 using namespace std;
 
 template<typename KeyType, typename ValueType, typename WeightType>
 class grafo {
     using VertexType = size_t;
     using AdjacentType = pair<VertexType, WeightType>;
+    using ItemType = pair<VertexType, ValueType>;
     // Lista de Vertices
-    unordered_map<KeyType, pair<VertexType, ValueType>> vertices;
+    unordered_map<KeyType, ItemType> vertices;
     // Vertices Adyacentes
-    vector<forward_list<AdjacentType>> adjacents;
+    vector<forward_list<AdjacentType>> adjacent_vertices;
+    KeyType get_key(VertexType vertex) {
+        auto it = find_if(begin(vertices), end(vertices), [vertex](const auto& item){
+           return item.second.first == vertex;
+        });
+        if (it == end(vertices))
+            throw runtime_error("key no encontrada");
+        return it->first;
+    }
 public:
     grafo() = default;
 
     bool add_vertex(pair<KeyType, ValueType> node) {
         // Buscar el vertex
         if (vertices.find(node.first) == end(vertices)) {
-            vertices.try_emplace(node.first, pair{size(vertices), node.second});
-            adjacents.emplace_back();
+            const auto [key, value] = node;
+            vertices.try_emplace(key, pair{size(adjacent_vertices), value});
+            adjacent_vertices.emplace_back();
             return true;
         }
         return false;
@@ -34,8 +48,65 @@ public:
     void add_edge(KeyType key_vertex1, KeyType key_vertex2, WeightType weight) {
         auto vx1 = vertices[key_vertex1].first;
         auto vx2 = vertices[key_vertex2].first;
-        adjacents[vx1].emplace_front(vx2, weight);
-        adjacents[vx2].emplace_front(vx1, weight);
+        adjacent_vertices[vx1].emplace_front(vx2, weight);
+        adjacent_vertices[vx2].emplace_front(vx1, weight);
+    }
+
+    template<typename UnaryFunction>
+    void bfs (KeyType key, UnaryFunction fun) {
+        queue<VertexType> q;
+        unordered_set<VertexType> visited;
+        auto is_visited= [&visited](VertexType vertex) {
+            return visited.find(vertex) != end(visited);
+        };
+        // Paso #1
+        auto fv = vertices[key].first;
+        q.push(fv);
+        visited.insert(fv);
+        // Paso #2
+        while(!q.empty()) {
+            // Paso #3
+            auto vx = q.front();
+            fun(get_key(vx));
+            // Paso #4
+            q.pop();
+            // Paso #5
+            for (auto [va, w]: adjacent_vertices[vx]) {
+                if (!is_visited(va)) {
+                    q.push(va);
+                    visited.insert(va);
+                }
+            }
+        }
+    }
+    template<typename UnaryFunction>
+    void dfs (KeyType key, UnaryFunction fun) {
+        stack<VertexType> s;
+        unordered_set<VertexType> visited;
+        // Paso #1
+        auto fv = vertices[key].first;
+        s.push(fv);
+        visited.insert(fv);
+        // Paso #2
+        while(!s.empty()) {
+            // Paso #3
+            auto vx_top = s.top();
+            // Paso # 4.0 Buscar el primer adjacente no visitado
+            const auto& adj = adjacent_vertices[vx_top];
+            auto it = find_if(begin(adj), end(adj), [&visited](auto item) {
+               return visited.find(item.first) == end(visited);
+            });
+            // Paso #4
+            if (it != end(adj)) {
+                s.push(it->first);
+                visited.insert(it->first);
+            }
+            // Paso #5
+            else {
+                fun(get_key(vx_top));
+                s.pop();
+            }
+        }
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -3,13 +3,26 @@
 
 int main() {
     grafo<string, int, int> g;
-    g.add_vertex({"A", 30});
-    g.add_vertex({"B", 40});
-    g.add_vertex({"C", 50});
+    g.add_vertex({"A", 10});
+    g.add_vertex({"E", 20});
+    g.add_vertex({"I", 30});
+    g.add_vertex({"O", 40});
+    g.add_vertex({"U", 50});
 
-    g.add_edge("A", "B", 2);
-    g.add_edge("C", "B", 2);
+    g.add_edge("A", "E", 10);
+    g.add_edge("E", "I", 7);
+    g.add_edge("E", "O", 8);
+    g.add_edge("I", "O", 2);
+    g.add_edge("I", "U", 5);
 
+    g.bfs("A", [](auto key) {
+        cout << key << " ";
+    });
+    cout << endl;
+    g.dfs("A", [](auto key) {
+        cout << key << " ";
+    });
+    cout << endl;
 
     return 0;
 }
